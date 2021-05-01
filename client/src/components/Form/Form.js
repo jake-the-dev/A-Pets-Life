8faca51071459f7228c67f835071175b5a73d447
Jake Webster <jakepeterwebster@gmail.com>
2021-05-01 10:04:10 +1000

    create new post form created

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+// import FileBase from "react-file-base64"; // TODO conflicts with npm packages
 
 import useStyles from "./styles";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "", // an image we can upload later
+  });
   const classes = useStyles();
 
-  return <h1>FORM</h1>;
+  const handleSubmit = () => {}; // function from previous projects
+
+  const clear = () => {};
+  // for each field use a spread operator of the state so the data will persist on change while only changing only that specific field.
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Create a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(event) =>
+            setPostData({ ...postData, creator: event.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(event) =>
+            setPostData({ ...postData, title: event.target.value })
+          }
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(event) =>
+            setPostData({ ...postData, message: event.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(event) =>
+            setPostData({ ...postData, tags: event.target.value })
+          }
+        />
+        <TextField
+          name="imageUpload"
+          variant="outlined"
+          label="Image Upload TODO"
+          fullWidth
+          value={postData.imageUpload}
+          onChange={(event) =>
+            setPostData({ ...postData, imageUpload: event.target.value })
+          }
+        />
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
